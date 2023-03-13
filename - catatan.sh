4a1419b52c5ae4f0aff159a47c91155a96c6b466
Harwinsyah <haris.harwinsyah@gmail.com>
2023-03-13 15:34:31 +0800

    first commit

---
+++ - catatan.sh
@@ -0,0 +1,8 @@
+go mod init nama_file
+
+go get github.com/Harwinsyah/go-say-hello
+
+git init 
+git add .
+git commit -m "text"
+git push -u origin main
---
+++ go.mod
@@ -0,0 +1,5 @@
+module github.com/Harwinsyah/app-say-hello
+
+go 1.20
+
+require github.com/Harwinsyah/go-say-hello/v2 v2.0.0 // indirect
